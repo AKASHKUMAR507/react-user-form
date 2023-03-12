231f19abf37e4525a5af8ac3d5358bc1f0671673
Akash kumar <65840057+AKASHKUMAR507@users.noreply.github.com>
2023-03-13 01:23:05 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -12,20 +12,24 @@ function App() {
   } = useForm();
 
   const onSubmit = (data) => {
-    console.log(data);
+    // console.table(data);
+   
+      document.write(`Name:- ${data.name} <br>`);
+      document.write(`Date of Birth:- ${data.date_of_birth} <br>`);
+      document.write(`Email:- ${data.email} <br>`);
+      document.write(`Phone Number:- ${data.phone} <br>`);
+
+    // let userData = JSON.stringify(data)
+    // document.write(userData);
+
     reset();
   };
-
-
-  // console.log(watch());
-
-  // console.log(errors.name)
 
   return (
     <div className="container pt-5">
       <div className="row justify-content-sm-center pt-5">
         <div className="col-sm-6 shadow round pb-3">
-          <h1 className="text-center pt-3 text-secondary">Example Form</h1>
+          <h1 className="text-center pt-3 text-secondary">User-Form</h1>
           <form onSubmit={handleSubmit(onSubmit)}>
             <div className="form-group">
               <label className="col-form-label">Name:</label>
@@ -42,43 +46,31 @@ function App() {
               )}
             </div>
             <div className="form-group">
-              <label className="col-form-label">Age:</label>
+              <label className="col-form-label">Date:</label>
               <input
-                type="text"
-                className={`form-control ${errors.age && "invalid"}`}
-                {...register("age", {
-                  required: "Age is Required",
-                  min: {
-                    value: 13,
-                    message: "Minimum Required age is 13",
-                  },
-                  max: {
-                    value: 65,
-                    message: "Maximum allowed age is 65",
-                  },
-                  pattern: {
-                    value: /^[0-9]*$/,
-                    message: "Only numbers are allowed",
-                  }
+                type="date"
+                className={`form-control ${errors.date && "invalid"}`}
+                {...register("date_of_birth", {
+                  required: "Date is Required",
                 })}
                 onKeyUp={() => {
-                  trigger("age");
+                  trigger("date");
                 }}
               />
-              {errors.age && (
-                <small className="text-danger">{errors.age.message}</small>
-              )}
+              
             </div>
             <div className="form-group">
               <label className="col-form-label">Email:</label>
               <input
                 type="text"
                 className={`form-control ${errors.email && "invalid"}`}
-                {...register("email", { required: "Email is Required" ,
-                pattern: {
-                  value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
-                  message: "Invalid email address",
-                }})}
+                {...register("email", {
+                  required: "Email is Required",
+                  pattern: {
+                    value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                    message: "Invalid email address",
+                  }
+                })}
                 onKeyUp={() => {
                   trigger("email");
                 }}
@@ -92,51 +84,32 @@ function App() {
               <input
                 type="text"
                 className={`form-control ${errors.phone && "invalid"}`}
-                {...register("phone", { required: "Phone is Required",
-                pattern: {
-                  value: /^\s*(?:\+?(\d{1,3}))?[-. (]*(\d{3})[-. )]*(\d{3})[-. ]*(\d{4})(?: *x(\d+))?\s*$/,
-                  message: "Invalid phone no",
-                },
-               })}
-               onKeyUp={() => {
-                trigger("phone");
-              }}
+                {...register("phone", {
+                  required: "Phone is Required",
+                  pattern: {
+                    value: /^\s*(?:\+?(\d{1,3}))?[-. (]*(\d{3})[-. )]*(\d{3})[-. ]*(\d{4})(?: *x(\d+))?\s*$/,
+                    message: "Invalid phone no",
+                  },
+                })}
+                onKeyUp={() => {
+                  trigger("phone");
+                }}
               />
               {errors.phone && (
                 <small className="text-danger">{errors.phone.message}</small>
-              )}
-            </div>
-            <div className="form-group">
-              <label className="col-form-label">Message:</label>
-              <textarea
-                className={`form-control ${errors.message && "invalid"}`}
-                {...register("message", { required: "Message is Required",
-                minLength: {
-                  value: 10,
-                  message: "Minimum Required length is 10",
-                },
-                maxLength: {
-                  value: 50,
-                  message: "Maximum allowed length is 50 ",
-                }
-               })}
-               onKeyUp={() => {
-                trigger("message");
-              }}
-              ></textarea>
-              {errors.message && (
-                <small className="text-danger">{errors.message.message}</small>
               )}
             </div>
             <input
               type="submit"
               className="btn btn-primary my-3"
               value="Send message"
+              formTarget="responsePage"
             />
           </form>
         </div>
       </div>
     </div>
+    
   );
 }
 
